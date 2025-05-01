fb86ad0ab551b8994b745ebc803f59ec6e82391b
boy indi prasetya <boyindi1905@gmail.com>
2025-05-01 12:21:46 +0700

    fourtyfy, login logout

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\View\View;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (): View {
+    return view('pages.auth.auth-login'); // ganti sesuai view kamu
 });
-Route::get('/dashboard', function () {
-    return view('pages.dashboard'['type => dashboard']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.dashboard', ['type_menu' => 'dashboard']);
+    })->name('home');
+
+// Route::resource('users',UsersController::class);
+
 });
